cad644712cf7689b1e105bceff1e329bf0d0a681
Charles Allen <charlesa@nu.ac.th>
2017-09-05 04:37:42 +0700

    Teacher - Save problem cases to db

--- Sources/App/Controllers/ProblemsController.swift
+++ Sources/App/Controllers/ProblemsController.swift
@@ -148,7 +148,24 @@ final class ProblemsController {
     
     /// POST /problems/:id/cases/new
     func problemCaseNewSubmit(request: Request) throws -> ResponseRepresentable {
-        return try render("Teacher/problem-case-new", for: request, with: view)
+        let problem = try request.parameters.next(Problem.self)
+        
+        guard
+            let problemId = problem.id,
+            let visibility = request.data["visibility"]?.string
+        else {
+            throw Abort.badRequest
+        }
+        
+        let visible = (visibility == "display")
+        let caseInput = request.data["case_input"]?.string ?? ""
+        let caseOutput = request.data["case_output"]?.string ?? ""
+        
+        // Save & continue
+        let problemCase = ProblemCase(input: caseInput, output: caseOutput, visible: visible, problemID: problemId)
+        try problemCase.save()
+        
+        return Response(redirect: "/problems/\(problemId.string!)/cases/new")
     }
 
 }
