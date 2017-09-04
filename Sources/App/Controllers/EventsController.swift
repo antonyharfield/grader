7fcf6036bfb5250917a3a3708f5905d04e05580b
Charles Allen <charlesa@nu.ac.th>
2017-09-05 00:52:28 +0700

    Teacher - Collect data for new event and save to DB

--- Sources/App/Controllers/EventsController.swift
+++ Sources/App/Controllers/EventsController.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Vapor
 import HTTP
 
@@ -37,8 +38,44 @@ final class EventsController: ResourceRepresentable {
     }
     
     /// GET /events/new
-    func new(request: Request) throws -> ResponseRepresentable {
-        return try render("event-new", for: request, with: view)
+    func eventNew(request: Request) throws -> ResponseRepresentable {
+        return try render("Teacher/event-new", for: request, with: view)
     }
     
+    func eventNewSubmit(request: Request) throws -> ResponseRepresentable {
+        guard
+            let userId = request.user?.id,
+            let name = request.data["name"]?.string
+        else {
+            throw Abort.badRequest
+        }
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd HH:mm"
+
+        // TBD: How do we handle invalid dates? (I think I'm just consuming them as nil)
+        let startsAt = request.data["starts_at"]?.string.flatMap { raw in formatter.date(from: raw) }
+        let endsAt = request.data["ends_at"]?.string.flatMap { raw in formatter.date(from: raw) }
+        let languageRestriction = request.data["language_restriction"]?.string.flatMap { raw in Language(rawValue: raw) }
+        
+        let event = Event(
+            name: name,
+            userID:userId,
+            startsAt: startsAt,
+            endsAt: endsAt,
+            languageRestriction: languageRestriction
+        )
+        try event.save()
+        
+        return Response(redirect: "/events/\(event.id!.string!)/problems/new")
+    }
+    
+    func eventProblemNew(request: Request) throws -> ResponseRepresentable {
+        return "PLACEHOLDER"
+    }
+
+    func eventProblemNewSubmit(request: Request) throws -> ResponseRepresentable {
+        return "PLACEHOLDER"
+    }
+
 }
--- Sources/App/TeacherRoutes.swift
+++ Sources/App/TeacherRoutes.swift
@@ -11,7 +11,11 @@ final class TeacherRoutes: RouteCollection {
     func build(_ builder: RouteBuilder) throws {
                 
         let eventsController = EventsController(view)
-        builder.get("events/new", handler: eventsController.new)
+        builder.get("events/new", handler: eventsController.eventNew)
+        builder.post("events/new", handler: eventsController.eventNewSubmit)
+        
+        builder.get("events", Event.parameter, "problems/new", handler: eventsController.eventProblemNew)
+        builder.post("events", Event.parameter, "problems/new", handler: eventsController.eventProblemNewSubmit)
         
     }
 }
