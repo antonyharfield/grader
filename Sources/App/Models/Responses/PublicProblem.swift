69a232f99c39df4e8c49b24867f3d04ce404e93b
Antony Harfield <antonyharfield@gmail.com>
2018-11-28 23:50:35 +0700

    Add problems to topic items

---
+++ Sources/App/Models/Responses/PublicProblem.swift
@@ -0,0 +1,7 @@
+import Vapor
+
+struct PublicProblem: Content {
+    var id: Int
+    var name: String
+    var description: String
+}
--- Sources/App/Web/Controllers/APIController.swift
+++ Sources/App/Web/Controllers/APIController.swift
@@ -5,6 +5,7 @@ extension APIController: RouteCollection {
     func boot(router: Router) throws {
         let authedRouter = router.grouped(SessionAuthenticationMiddleware())
         authedRouter.get("api", "submissions", use: getSubmissions)
+        authedRouter.get("api", "problems", use: getProblems)
     }
 }
 
@@ -30,6 +31,40 @@ final class APIController {
             return request.future(SubmissionsResponse(submissions: submissions))
         }
     }
+    
+    func getProblems(request: Request) throws -> Future<ProblemsResponse> {
+        let keywordRaw = request.query[String.self, at: "keyword"]
+        
+        guard let keyword = keywordRaw, keyword.count > 2 else {
+            return request.future(ProblemsResponse(problems: []))
+        }
+        
+        let sql: String = [
+            "SELECT id, name, description FROM problems",
+            "WHERE name like ?",
+            "ORDER BY id DESC"
+            ].joined(separator: " ")
+        let params = ["%\(keyword)%"]
+        
+        let problemsFuture: Future<[[MySQLColumn: MySQLData]]> = request.withPooledConnection(to: .mysql) { (conn: MySQLDatabase.Connection) in
+            return conn.raw(sql).binds(params).all()
+        }
+        
+        return problemsFuture.map(parseProblems).flatMap { problems in
+            return request.future(ProblemsResponse(problems: problems))
+        }
+    }
+    
+    private func parseProblems(rows: [[MySQLColumn: MySQLData]]) throws -> [PublicProblem] {
+        var results: [PublicProblem] = []
+        for row in rows {
+            let id = try row.firstValue(forColumn: "id")!.decode(Int.self)
+            let name = try row.firstValue(forColumn: "name")!.decode(String.self)
+            let description = try row.firstValue(forColumn: "description")!.decode(String.self)
+            results.append(PublicProblem(id: id, name: name, description: description))
+        }
+        return results
+    }
 }
 
 struct SubmissionsRequest: Content {
@@ -38,4 +73,8 @@ struct SubmissionsRequest: Content {
 }
 struct SubmissionsResponse: Content {
     let submissions: [Submission]
+}
+
+struct ProblemsResponse: Content {
+    let problems: [PublicProblem]
 }
